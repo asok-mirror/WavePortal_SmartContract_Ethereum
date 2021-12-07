ea182c6edb64e0c9950d4170242a27772adbfe84
asokkalidass kalisamy <asok.kalidass@gmail.com>
2021-12-06 18:56:13 -0800

    updated the contract

--- contracts/wavePortal.sol
+++ contracts/wavePortal.sol
@@ -1,4 +1,3 @@
-
 // SPDX-License-Identifier: UNLICENSED
 
 pragma solidity ^0.8.0;
@@ -6,7 +5,20 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    //state variable
+    uint256 totalWaves;
+
     constructor() {
         console.log("I am an empty waveportal contract");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waves!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("total number of people waves is: %s", totalWaves);
+        return totalWaves;
     }
 }
