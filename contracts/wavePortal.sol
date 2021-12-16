c2195cfd360190f448285111d8ef37f82059a7af
asokkalidass kalisamy <asok.kalidass@gmail.com>
2021-12-16 11:20:35 -0800

    updated the contract to include data during wave

--- contracts/wavePortal.sol
+++ contracts/wavePortal.sol
@@ -8,19 +8,42 @@ contract WavePortal {
     //state variable
     uint256 totalWaves;
     mapping(address => uint256) public wavesTrackerByAddress;
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    struct Wave {
+        address waver;
+        string message;
+        uint256 timestamp;
+    }
 
     constructor() {
         console.log("I am an empty waveportal contract");
     }
 
-    function wave() public {
+    Wave[] waves;
+
+    function wave(string calldata _message) external {
         totalWaves += 1;
-        console.log("%s has waved", msg.sender);
+        console.log("%s waved w/ message %s", msg.sender, _message);
+
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        emit NewWave(msg.sender, block.timestamp, _message);
 
         //track the wave count by address
-        wavesTrackerByAddress[msg.sender] = getCurrentWaveCountBySender(msg.sender) + 1;
+        wavesTrackerByAddress[msg.sender] =
+            getCurrentWaveCountBySender(msg.sender) +
+            1;
 
-        console.log("%s has totally waved %s times", msg.sender,  wavesTrackerByAddress[msg.sender]);
+        console.log(
+            "%s has totally waved %s times",
+            msg.sender,
+            wavesTrackerByAddress[msg.sender]
+        );
+    }
+
+    function getAllWaves() external view returns (Wave[] memory) {
+        return waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
@@ -28,7 +51,11 @@ contract WavePortal {
         return totalWaves;
     }
 
-    function getCurrentWaveCountBySender(address sender) private view returns (uint256) {
-        return  wavesTrackerByAddress[sender];
+    function getCurrentWaveCountBySender(address sender)
+        public
+        view
+        returns (uint256)
+    {
+        return wavesTrackerByAddress[sender];
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -10,23 +10,24 @@ const main = async () => {
 
     let waveCount;
     waveCount = await waveContract.getTotalWaves();
+    console.log(waveCount.toNumber());
 
     //invoke wave by creater address - 1st run
-    let wave = await waveContract.wave();
+    let wave = await waveContract.wave('Hey!! all the best for your solidity learning');
     await wave.wait()
 
     waveCount =  await waveContract.getTotalWaves();
 
      //invoke wave by creater address - 2nd run
-     wave = await waveContract.wave();
-     await wave.wait()
+     wave = await waveContract.wave('Test Wave');
+     await wave.wait();
 
     //invoke the contract by different address
-    wave = await waveContract.connect(randomPerson).wave();
+    wave = await waveContract.connect(randomPerson).wave('Welcome!! to web3.0 world');
     await wave.wait();
 
-    waveCount = await waveContract.getTotalWaves();
-    
+    let allWaves = await waveContract.getAllWaves();
+    console.log(allWaves);
 };
 
 const runMain = async () => {
